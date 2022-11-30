a14fe0d7bf3926879a145f91bcd9cc660576a326
Michal Martinek <m_martinek@utb.cz>
2022-11-30 23:08:57 +0100

    5. Task: Parse the JSON response with Moshi

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -16,5 +16,11 @@
  */
 
 package com.example.android.marsrealestate.network
+import com.squareup.moshi.Json
 
-class MarsProperty()
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double
+)
